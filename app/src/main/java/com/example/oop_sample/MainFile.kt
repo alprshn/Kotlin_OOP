3168c78009f9aac48116f4a313e7f10054d02107
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-03-28 17:10:24 +0300

    added

--- app/src/main/java/com/example/oop_sample/MainFile.kt
+++ app/src/main/java/com/example/oop_sample/MainFile.kt
@@ -1,9 +1,30 @@
 package com.example.oop_sample
 
 fun main(){
-    val degreestofahrenheit = DegreesToFahrenheit()
+    var a = 6
+    println("Mük Sayı = $a")
+    var b =0
 
-    val sonuc = degreestofahrenheit.convertDegress(30.0)
-    println(sonuc)
+    for ( i in 1..a-1){
+
+        if (a % i ==0){
+            println("Pozitif Bölenleri = $i")
+            b= i +b
+        }
+    }
+    println("Pozitif Bölenleri Toplamı = $b")
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
